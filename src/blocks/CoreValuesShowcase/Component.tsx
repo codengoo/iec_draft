bb7c070e72266888fb00af839dfb61374236d847
NghiaDT <nghiadt@ieccorp.vn>
2026-05-25 19:41:39 +0700

    style: format className prop for improved readability in ValueCard component

--- src/blocks/CoreValuesShowcase/Component.tsx
+++ src/blocks/CoreValuesShowcase/Component.tsx
@@ -154,7 +154,9 @@ const ValueCard: React.FC<{
 
   return (
     <motion.li
-      className={positionStyle ? 'absolute list-none pointer-events-auto hover:z-50' : 'w-full list-none'}
+      className={
+        positionStyle ? 'absolute list-none pointer-events-auto hover:z-50' : 'w-full list-none'
+      }
       style={{ opacity: animOpacity, y: animY, ...positionStyle }}
       initial={{ rotate: rotateInit }}
       animate={{ rotate: rotateInit }}
